385a8f58dc725e84fac0498c6a7b13c4ea58bd19
yangchunqi <yangchunqi@guazi.com>
2018-04-20 22:16:13 +0800

    init

---
+++ src/main/java/com/yang/bilisniper/component/TestJob.java
@@ -0,0 +1,16 @@
+package com.yang.bilisniper.component;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ChunqiYang (yangchunqi@guazi.com)
+ */
+@Component
+public class TestJob {
+
+    @Scheduled(cron = "0/10 * * * * ?")
+    public void testJob() {
+        System.out.println("hello world");
+    }
+}
