5199329aabbe6c570e54ebe41473ed8a521795b5
yangchunqi <yangchunqi@guazi.com>
2018-04-20 21:38:02 +0800

    Initial commit

---
+++ src/main/java/com/yang/bilisniper/BilisniperApplication.java
@@ -0,0 +1,12 @@
+package com.yang.bilisniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilisniperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilisniperApplication.class, args);
+	}
+}
