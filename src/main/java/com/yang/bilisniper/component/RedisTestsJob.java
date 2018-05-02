d1c59e79670e0c0376f4956a28ddeaceb9c904d2
yangchunqi <yangchunqi@guazi.com>
2018-05-02 19:08:25 +0800

    finish redis config

---
+++ src/main/java/com/yang/bilisniper/component/RedisTestsJob.java
@@ -0,0 +1,27 @@
+package com.yang.bilisniper.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by ChunqiYang (yangchunqi@guazi.com)
+ */
+@Component
+public class RedisTestsJob {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisTestsJob.class);
+
+    @Resource(name = "stringRedisTemplate")
+    private RedisTemplate redisTemplate;
+
+    //@Scheduled(cron = "0/10 * * * * ?")
+    public void redisSet() {
+        //redisTemplate.opsForList().leftPush("springBootTest", "test");
+        //System.out.println(redisTemplate.opsForList().leftPop("springBootTest"));
+        redisTemplate.opsForValue().append("test1", "test");
+    }
+}
